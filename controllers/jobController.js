f79986b610fe9e26ac73a68db2e5bc0f2454db66
chmubarak <chmohamedmubarak@gmail.com>
2023-01-10 13:34:51 +0530

    add filter to jobs

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -54,41 +54,73 @@ const getJobDetail = async (req, res, next) => {
   }
 };
 
-const getAllJobs = async (req, res) => {
+const getAllJobs = async (req, res, next) => {
   const limit = req.query.limit || 5;
   const skip = req.query.skip || 0;
+  const experience = Number(req.query.experience);
+  //checking skills query have multiple value or not
+  const skills = Array.isArray(req.query.skills)
+    ? [...req.query.skills]
+    : [req.query.skills];
+  const pipeline = [];
+
+  // conditionally checking if filtering is enabled or not for skill
+  if (skills) {
+    console.log(skills);
+    pipeline.push({
+      $match: {
+        skills: {
+          $in: skills,
+        },
+      },
+    });
+  }
+
+  // conditionally checking if filtering is enabled or not for experience
+  if (experience || experience === 2) {
+    pipeline.push({
+      $match: {
+        experience: {
+          $lte: experience,
+        },
+      },
+    });
+  }
+
+  pipeline.push(
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "author",
+        foreignField: "_id",
+        as: "author",
+      },
+    },
+    {
+      $unwind: {
+        path: "$author",
+      },
+    },
+    {
+      $project: {
+        "author.password": false,
+      },
+    }
+  );
+
   try {
-    const jobs = await Job.aggregate([
-      {
-        $sort: {
-          createdAt: -1,
-        },
-      },
-      {
-        $skip: skip,
-      },
-      {
-        $limit: limit,
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "author",
-          foreignField: "_id",
-          as: "author",
-        },
-      },
-      {
-        $unwind: {
-          path: "$author",
-        },
-      },
-      {
-        $project: {
-          "author.password": false,
-        },
-      },
-    ]);
+    const jobs = await Job.aggregate(pipeline);
     return res.status(200).json(jobs);
   } catch (err) {
     next(err);
